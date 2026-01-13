548cf7ef4560fb99eade7d09534047d33e5b3663
ostregag <193748303+ostregag@users.noreply.github.com>
2026-01-13 18:32:37 +0100

    License (#4)
    * proper licensing and simple style

--- site/panel/index.php
+++ site/panel/index.php
@@ -1,15 +1,17 @@
 <?php
-require "check.php";
+require "check.php"; // this is required for every protected site 
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Protected Site</title>
+    <h1> this is your protected site - accesible only to logged in users</h1>
 </head>
 <body>
-  <a href="wyloguj.php" method="POST">logout</a>  
+  <a href="logout.php" method="POST">logout</a>  
 </body>
 
 
--- site/panel/wyloguj.php
+++
@@ -1,3 +0,0 @@
-<?php
-require $_SERVER['backend'] . '/wyloguj.php';
-?>
